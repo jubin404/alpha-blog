578f0fa7c6ad7013b2f08e941b83c3d671698289
jubin404 <jubin.jose@terrificminds.com>
2023-10-10 17:54:10 +0530

    add user section and profile picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = {size: 80})
+    email = user.email.downcase
+    hash = Digest::MD5.hexdigest(email)
+    size = options[:size]
+    gravatr_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatr_url, alt: user.username)
+  end
 end
