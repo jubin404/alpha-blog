50127dc81a972fc02258d128a530d1e21d068fce
jubin404 <jubin.jose@terrificminds.com>
2023-10-11 13:04:45 +0530

    add create and destroy users with sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = 'Logged in succesfully'
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:alert] = 'Wrong credentials'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+end
