babba193d54ef4ee458102affe60267f854ceeb8
jubin404 <jubin.jose@terrificminds.com>
2023-10-11 15:22:56 +0530

    add authentication helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles  
   end
 
@@ -14,12 +15,9 @@ class UsersController < ApplicationController
   end  
 
   def edit
-  @user = User.find(params[:id])  
   end
 
   def update
-    @user = User.find(params[:id]) 
-
     if @user.update(user_params)
       flash[:notice] = "Your account was successfully updated"
       redirect_to @user
@@ -43,5 +41,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
