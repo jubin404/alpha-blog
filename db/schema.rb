d63103232ce34ecc1d52b73abba21495a76e8d17
jubin404 <jubin.jose@terrificminds.com>
2023-10-12 10:58:34 +0530

    add admin user functionailty

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_042817) do
+ActiveRecord::Schema.define(version: 2023_10_12_045013) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2023_10_10_042817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
