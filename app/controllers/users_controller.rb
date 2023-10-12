a8158618a9b9c807fff4394f1794a6eb207d8a21
jubin404 <jubin.jose@terrificminds.com>
2023-10-12 09:50:08 +0530

    add action restrictions at user level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
 
   def show
     @articles = @user.articles  
@@ -45,5 +47,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit your own account"
+      redirect_to @user
+    end
   end
 end
