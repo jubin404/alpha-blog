f70a019f1d9d7ebda7575987f8bd53f99068db05
jubin404 <jubin.jose@terrificminds.com>
2023-10-13 17:30:12 +0530

    add many to many association between articles and categories in database

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5, maximum: 10 }
   validates_uniqueness_of :name
+  
+  has_many :article_categories
+  has_many :articles, through: :article_categories
 end
