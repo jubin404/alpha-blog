90f0e15c42a74687ba802161f4a35e52ea096a94
jubin404 <jubin.jose@terrificminds.com>
2023-10-16 11:27:40 +0530

    add view and edit options to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,12 +16,27 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name was updated successfully"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles
   end
 
   private
